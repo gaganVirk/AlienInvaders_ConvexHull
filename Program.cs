e72291cc796adb94d44ab01cda6b40e2bc68a880
gaganVirk <deep.virk@outlook.co.nz>
2019-09-25 13:11:41 +1200

    Created convex hull algorithm on console

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienInvaders_ConvexHull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> points = new List<Point>();
+            List<Point> hull = new List<Point>();
+            Point pivot = new Point(int.MaxValue, int.MaxValue);
+
+            TextReader stdIn = Console.In;
+            Console.SetIn(new StreamReader("data.txt"));
+
+            int nA = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nA; i++)
+            {
+                string input = Console.ReadLine();
+                string[] array = input.Split(' ');
+                int valOne = int.Parse(array[0]);
+                int valTwo = int.Parse(array[1]);
+                if (pivot.X > valOne)
+                {
+                    if (pivot.Y > valOne)
+                        pivot = new Point(valOne, valTwo);
+                }
+                points.Add(new Point(valOne, valTwo));
+            }
+            points.Sort(new RadSort(pivot));
+         
+                hull.Clear();
+                hull.Add(pivot);
+                hull.Add(points[0]);
+                points.Remove(points[0]);
+
+                while (points.Count > 0)
+                {
+                    hull.Add(points[0]);
+                    points.Remove(points[0]);
+
+                    while (!IsValidHull())
+                    {
+                        hull.Remove(hull[hull.Count - 2]);
+                    }
+                }
+                hull.Add(pivot);
+           
+            for (int i = hull.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(hull[i].X + " " + hull[i].Y);
+            }
+
+            Console.SetIn(stdIn);
+            Console.ReadLine();
+
+
+            bool IsValidHull()
+            {
+                if(hull.Count < 3)
+                {
+                    return true;
+                }
+
+                Point a = hull[hull.Count - 3];
+                Point b = hull[hull.Count - 2];
+                Point c = hull[hull.Count - 1];
+                int cw = a.X * b.Y - b.X * a.Y + b.X * c.Y - c.X * b.Y + c.X * a.Y - a.X * c.Y;
+                return cw < 0;
+            }
+        }
+    }
+}
---
+++ RadSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienInvaders_ConvexHull
+{
+    class RadSort : IComparer<Point>
+    {
+        Point a;
+
+        public RadSort(Point a)
+        {
+            this.a = a;
+        }
+
+        public int Compare(Point b, Point c)
+        {
+            int cw = a.X * b.Y - b.X * a.Y + b.X * c.Y - c.X * b.Y + c.X * a.Y - a.X * c.Y;
+            if(cw == 0)
+            {
+                double d1 = GetDistTo(b);
+                double d2 = GetDistTo(c);
+                return d1.CompareTo(d2);
+            }
+            return cw;
+        }
+
+        private double GetDistTo(Point point)
+        {
+            int dX = point.X - a.X;
+            int dY = point.Y - a.Y;
+            return Math.Sqrt(dX * dX + dY * dY);
+        }
+    }
+}
